b11cb75f02db83a0024b7437f062d2db08774b38
Adedayo Oladele <oladele.adedayo@yahoo.com>
2025-11-29 18:42:49 +0100

    fix state mismatch error caused by auth client defining allowedOrigins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -21,11 +21,15 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      redirectURI:
+        "https://blog-app-bootcamp.vercel.app/api/auth/callback/github",
     },
     google: {
       prompt: "select_account",
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectURI:
+        "https://blog-app-bootcamp.vercel.app/api/auth/callback/google",
     },
   },
   trustedOrigins: [
